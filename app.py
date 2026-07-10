a58e2029c59688de89d9e55e17720566f2e7bd15
Om Jawanjalkar <omjawanjalkar@gmail.com>
2026-07-10 20:37:27 +0530

    Update Streamlit app

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 
-
 # Load the trained model
 model = tf.keras.models.load_model('model.h5')
 
